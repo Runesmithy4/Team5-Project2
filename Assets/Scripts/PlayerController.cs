9c54678fd9cef1c0c53fd42512cf4d2b20c49c13
alexbeifeld <alexbeifeld@gmail.com>
2021-09-27 14:07:59 -0400

    Added Projectile firing mechanic for the player ship

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Public variables used for instantiated lazers
+    public GameObject lazerPrefab;
+    public Transform lazerSpawnOne;
+    public Transform lazerSpawnTwo;
+    public float lazerSpeed = 30;
+    public float lifetime = 3;
+
+
     public float sideMovement;
     
     void Update()
@@ -16,5 +24,40 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x + sideMovement, transform.position.y);
         }
+
+        //When player hits spacebar the ship will shoot calling the Fire() function
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+
+        
+    }
+
+    //Instantiates two lazer projectiles at two different starting locations (both are children of the parent ship). Then starts a IEnumerator to destroy the object after some time.
+    private void Fire()
+    {
+        GameObject lazerOne = Instantiate<GameObject>(lazerPrefab);
+        lazerOne.transform.position = lazerSpawnOne.transform.position;
+        Rigidbody lazerOneRB = lazerOne.GetComponent<Rigidbody>();
+        lazerOneRB.velocity = Vector3.back * lazerSpeed;
+
+        StartCoroutine(DestroyLazerAfterTime(lazerOne, lifetime));
+        
+        GameObject lazerTwo = Instantiate<GameObject>(lazerPrefab);
+        lazerTwo.transform.position = lazerSpawnTwo.transform.position;
+        Rigidbody lazerTwoRB = lazerTwo.GetComponent<Rigidbody>();
+        lazerTwoRB.velocity = Vector3.back * lazerSpeed;
+
+        StartCoroutine(DestroyLazerAfterTime(lazerTwo, lifetime));
+
+    }
+
+    //Destroys object after a set amount of time.
+    private IEnumerator DestroyLazerAfterTime(GameObject lazer, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Destroy(lazer);
     }
 }
